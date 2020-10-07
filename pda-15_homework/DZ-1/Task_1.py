2faf8b5f4c53e6696c3707a38ba39bf301203fab
GMutovin <gleb9610@gmail.com>
2020-10-07 18:16:19 +0300

    Task 1 completed

--- pda-15_homework/DZ-1/Task_1.py
+++ pda-15_homework/DZ-1/Task_1.py
@@ -1,1 +1,6 @@
-#Task_1
+if len(phrase_1) > len(phrase_2):
+    print("Фраза 1 длиннее фразы 2")
+elif len(phrase_1) < len(phrase_2):
+    print("Фраза 2 длиннее фразы 1")
+else:
+    print("Фразы равной длины")
