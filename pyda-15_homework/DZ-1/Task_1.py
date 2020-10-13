848ad5f1c14f50cc9431b589319268d1568631f6
GMutovin <gleb9610@gmail.com>
2020-10-13 15:08:00 +0700

    Homework #1 done

--- pyda-15_homework/DZ-1/Task_1.py
+++
@@ -1,7 +0,0 @@
-#Домашнее задание по теме «Основы Python» - Задание № 1
-if len(phrase_1) > len(phrase_2):
-    print("Фраза 1 длиннее фразы 2")
-elif len(phrase_1) < len(phrase_2):
-    print("Фраза 2 длиннее фразы 1")
-else:
-    print("Фразы равной длины")
